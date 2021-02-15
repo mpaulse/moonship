556d24fd020981f5f93bc8a8842bb35a97884972
Marlon Paulse <mpaulse@gmail.com>
2021-02-15 21:38:21 +0200

    Implemented order book management.

--- moonship/market.py
+++ moonship/market.py
@@ -24,13 +24,14 @@
 
 import abc
 import asyncio
+import collections
 import sortedcontainers
 
 from .config import *
 from .data import *
 from .error import *
 from dataclasses import dataclass, field
-from typing import Union
+from typing import Iterator, Union
 
 __all__ = [
     "Market",
@@ -39,6 +40,7 @@ __all__ = [
     "MarketEvent",
     "MarketFeedSubscriber",
     "MarketStatusEvent",
+    "OrderBookEntriesView",
     "OrderBookEntry",
     "OrderBookItemAddedEvent",
     "OrderBookItemRemovedEvent",
@@ -164,14 +166,16 @@ class MarketFeed(abc.ABC):
     def raise_event(self, event: MarketEvent) -> None:
         for sub in self.subscribers:
             func = None
-            if isinstance(event, OrderBookInitEvent):
-                func = sub.on_order_book_init(event)
-            elif isinstance(event, OrderBookItemAddedEvent):
+            if isinstance(event, OrderBookItemAddedEvent):
                 func = sub.on_order_book_item_added(event)
             elif isinstance(event, OrderBookItemRemovedEvent):
                 func = sub.on_order_book_item_removed(event)
             elif isinstance(event, TradeEvent):
                 func = sub.on_trade(event)
+            elif isinstance(event, TickerEvent):
+                func = sub.on_ticker(event)
+            elif isinstance(event, OrderBookInitEvent):
+                func = sub.on_order_book_init(event)
             elif isinstance(event, MarketStatusEvent):
                 func = sub.on_market_status_update(event)
             if func is not None:
@@ -179,20 +183,69 @@ class MarketFeed(abc.ABC):
 
 
 class OrderBookEntry:
-    price: Amount
-    volume: Amount
-    orders: dict[str, LimitOrder]
+
+    def __init__(self, order: LimitOrder) -> None:
+        self._price = order.price
+        self._orders = {order.id: order}
+
+    @property
+    def price(self) -> Amount:
+        return self._price
+
+    @property
+    def volume(self) -> Amount:
+        volume = Amount(0)
+        for order in self._orders.values():
+            volume += order.volume
+        return volume
+
+
+class OrderBook:
+    bids = sortedcontainers.SortedDict[Amount, OrderBookEntry]()
+    asks = sortedcontainers.SortedDict[Amount, OrderBookEntry]()
+    order_entry_index: dict[str, OrderBookEntry] = {}
+
+    def add_order(self, order: LimitOrder) -> None:
+        entries = self.bids if order.action == OrderAction.BUY else self.asks
+        entry = entries.get(order.price)
+        if entry is None:
+            entry = OrderBookEntry(order)
+            entries[order.price] = entry
+        else:
+            entry._orders[order.id] = order
+        self.order_entry_index[order.id] = entry
+
+    def remove_order(self, order_id: str) -> None:
+        entry = self.order_entry_index.get(order_id)
+        if entry is not None:
+            del entry._orders[order_id]
+            del self.order_entry_index[order_id]
+
+
+class OrderBookEntriesView(collections.Mapping):
+
+    def __init__(self, entries: sortedcontainers.SortedDict[Amount, OrderBookEntry]) -> None:
+        self._entries = entries
+
+    def __getitem__(self, amount: Amount) -> OrderBookEntry:
+        return self._entries.get(amount)
+
+    def __len__(self) -> int:
+        return len(self._entries)
+
+    def __iter__(self) -> Iterator[Amount]:
+        return iter(self._entries)
 
 
 class Market:
 
-    def __init__(self, name: str, symbol: str, client: MarketClient, feed: MarketFeed):
+    def __init__(self, name: str, symbol: str, client: MarketClient, feed: MarketFeed) -> None:
         self._name = name
         self._symbol = symbol
         self._client = client
         self._feed = feed
-        self._bids = sortedcontainers.SortedSet[OrderBookEntry]()
-        self._asks = sortedcontainers.SortedSet[OrderBookEntry]()
+        self._current_price = Amount(0)
+        self._order_book = OrderBook()
         self._status = MarketStatus.ACTIVE
         self.subscribe_to_feed(MarketManager(self))
 
@@ -207,6 +260,30 @@ class Market:
     @property
     def status(self) -> MarketStatus:
         return self._status
+
+    @property
+    def current_price(self) -> Amount:
+        return self._current_price
+
+    @property
+    def bid_price(self) -> Amount:
+        return self._order_book.bids.peekitem(0)[0] if len(self._order_book.bids) > 0 else Amount(0)
+
+    @property
+    def ask_price(self) -> Amount:
+        return self._order_book.asks.peekitem(0)[0] if len(self._order_book.asks) > 0 else Amount(0)
+
+    @property
+    def spread(self) -> Amount:
+        return self.ask_price - self.bid_price
+
+    @property
+    def bids(self) -> OrderBookEntriesView:
+        return OrderBookEntriesView(self._order_book.bids)
+
+    @property
+    def asks(self) -> OrderBookEntriesView:
+        return OrderBookEntriesView(self._order_book.asks)
 
     async def get_tickers(self) -> list[Ticker]:
         if self._status == MarketStatus.DISABLED:
@@ -246,13 +323,30 @@ class MarketManager(MarketFeedSubscriber):
         self.market = market
 
     async def on_order_book_init(self, event: OrderBookInitEvent) -> None:
-        pass
+        self.market._status = event.status
+        for order in event.orders:
+            self.market._order_book.add_order(order)
 
     async def on_order_book_item_added(self, event: OrderBookItemAddedEvent) -> None:
-        pass
+        self.market._order_book.add_order(event.order)
 
     async def on_order_book_item_removed(self, event: OrderBookItemRemovedEvent) -> None:
-        pass
+        self.market._order_book.remove_order(event.order_id)
 
     async def on_market_status_update(self, event: MarketStatusEvent) -> None:
         self.market._status = event.status
+
+    async def on_trade(self, event: TradeEvent) -> None:
+        self.market._current_price = event.counter_amount / event.base_amount
+        self.market._order_book.remove_order(event.maker_order_id)
+        self.market._feed.raise_event(
+            TickerEvent(
+                timestamp=event.timestamp,
+                symbol=event.symbol,
+                ticker=Ticker(
+                    timestamp=event.timestamp,
+                    symbol=event.symbol,
+                    current_price=self.market.current_price,
+                    bid_price=self.market.bid_price,
+                    ask_price=self.market.ask_price,
+                    status=self.market.status)))
